eb81f47daa339240a9c4aa3132d6eccab8e29697
Terry <ytdxxt10@163.com>
2025-07-04 17:27:52 +0800

    fix:js修改为ts

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+
+type Config = import('hardhat/config').HardhatUserConfig;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config:Config = {
+  solidity: "0.8.28",
+};
+
+export default config;
